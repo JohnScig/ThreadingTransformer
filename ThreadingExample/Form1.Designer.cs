41933dd56d9613f3363cb348975eca131df88594
John Scigulinsky <john.scigulinsky@gmail.com>
2019-05-29 14:36:31 +0200

    commit

--- ThreadingExample/Form1.Designer.cs
+++ ThreadingExample/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 20);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(687, 82);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(101, 26);
+            this.btn_load.TabIndex = 1;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(580, 82);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(101, 26);
+            this.btn_cancel.TabIndex = 1;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 120);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_load;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn_cancel;
     }
 }
 
--- ThreadingExample/Form1.cs
+++ ThreadingExample/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,70 @@ namespace ThreadingExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int param = (int)e.Argument;
+            for (int i = 0; i <=100; i++)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                Thread.Sleep(param * 25);
+                backgroundWorker1.ReportProgress(i);
+
+                //if (i>10)
+                //{
+                //    throw new ArgumentException("Sum Ting Wong");
+                //}
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled");
+                progressBar1.Value = 0;
+                return;
+            }
+
+
+            if (e.Error == null)
+            {
+                MessageBox.Show($"Thread {Thread.CurrentThread.ManagedThreadId} ended");
+            }
+            else
+            {
+                MessageBox.Show($"Thread {Thread.CurrentThread.ManagedThreadId} crashed and burned\n" + e.Error.Message);
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+
+            int waitTime = 1;
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync(waitTime);
+
+            }
+        }
+
+        //tento môže volať GUI, ale nemá vytvárať vlastné vlákno
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
         }
     }
 }
